1ecfeb09bda7a2e9a35a9f4d78c13e09b1cfa744
TheGhostRider <rushidhanawade123@gmail.com>
2020-11-07 18:24:20 +0530

    Create info.py

---
+++ info.py
@@ -0,0 +1,18 @@
+
+firstClass = ("10:05")
+secondClass = ("11:15")
+thirdClass = ("12:25")
+fourthClass = ("14:05")
+#fifthClass = ("00:00")
+dummyClass = ("17:30")
+
+
+#enter links here
+Cs = ("https://meet.google.com/")
+Mcom = ("https://meet.google.com/")
+Bda = ("https://meet.google.com/")
+Me = ("https://meet.google.com/")
+Oc = ("https://meet.google.com/")
+MeLab = ("https://meet.google.com/")
+BdaLab = ("https://meet.google.com/")
+dummy = ("https://meet.google.com/")
