46da2b3779754b6688820c95c45ef53b9607dd40
TheGhostRider <rushidhanawade123@gmail.com>
2020-11-07 18:22:41 +0530

    Create join.py

---
+++ join.py
@@ -0,0 +1,33 @@
+import info
+import time
+
+def joinCs():
+    driver.get(Cs) #seconds
+    time.sleep(3900)
+    driver.quit()
+
+def joinMcom():
+    driver.get(Mcom)
+    time.sleep(3900)
+    driver.quit()
+
+def joinBda():
+    driver.get(Bda)
+    time.sleep(3900)
+    driver.quit()
+
+def joinMe():
+    driver.get(Me)
+    time.sleep(60) #seconds
+    time.sleep(3900)
+    driver.quit()
+
+def joinOc():
+    driver.get(Oc)
+    time.sleep(3600)
+    driver.quit()
+
+def joindummy():
+    driver.get(dummy)
+    time.sleep(1800)
+    driver.quit()
